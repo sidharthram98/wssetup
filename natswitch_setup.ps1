ea594cd46c0e5c0f3979cbbe448e53eed45750cb
Braulio Chavez <31143185+brauliomsft@users.noreply.github.com>
2022-06-23 11:29:46 -0500

    Update natswitch_setup.ps1

--- natswitch_setup.ps1
+++ natswitch_setup.ps1
@@ -1,5 +1,5 @@
 $vmname = "Ubuntu"
 New-VMSwitch -Name "NAT" -SwitchType Internal
-New-NetIPAddress -IPAddress 192.168.200.1 -PrefixLenght 24 -InterfaceAlias "vEthernet (NAT)"
+New-NetIPAddress -IPAddress 192.168.200.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NAT)"
 New-NetNat -Name NAT -InternalIPInterfaceAddressPrefix 192.168.200.0/24
 Get-VMNetworkAdapter -VMName $VMName | Connect-VMNetworkAdapter -SwitchName "NAT"
