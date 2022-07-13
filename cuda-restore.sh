62cd1df8f01a3f2004051e9c9fbb1308d596b165
sidharth <87812643+sidharthram98@users.noreply.github.com>
2022-07-13 18:08:12 +0530

    Create cuda-restore.sh

---
+++ cuda-restore.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get --purge remove "*cublas*" "cuda*" "nsight*" 
+sudo apt-get --purge remove "*nvidia*"
+sudo rm -rf /usr/local/cuda*
+sudo apt install gcc
+sudo apt-get install linux-headers-$(uname -r)
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.2-510.47.03-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.2-510.47.03-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
