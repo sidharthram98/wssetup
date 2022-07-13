464ec2bb8edf6c52604ed185459a15f9b05dbac9
sidharth <87812643+sidharthram98@users.noreply.github.com>
2022-07-13 11:23:59 +0530

    Create delete-image.sh

---
+++ delete-image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+flux uninstall -s
+echo "Flux hooks.. Removed!! 😶‍🌫️"
+sleep 5
+
+sudo microk8s kubectl delete deployments --all -n=default
+echo "Deployments.. Killed!! 🥶"
+sleep 5
+sudo microk8s enable gpu
+echo "Gpu enabled back again! 😥"
+sleep 5
+
+sudo microk8s ctr images rm ghcr.io/anicca-computer-vision-prod/cv_models_prod:$1
+echo "${1} Image removed 😎"
