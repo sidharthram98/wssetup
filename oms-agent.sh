dc6f477c70608e30bb02955be0cfcb2a1789b4eb
sidharth <87812643+sidharthram98@users.noreply.github.com>
2022-07-27 18:37:56 +0530

    Update oms-agent.sh

--- oms-agent.sh
+++ oms-agent.sh
@@ -2,7 +2,7 @@
 export $(grep -v '^#' .env | xargs)
 
 sudo snap install helm --classic 
-helm repo add microsoft https://microsoft.github.io/charts/repo
+helm repo add $OMS_REPO_NAME $OMS_REPO_URL
 
 
 helm install $STORE_NAME-logs --set omsagent.secret.wsid=a2a23cf2-b161-4631-85da-d9cf38e4553e,omsagent.secret.key=hymgpbf5F9EnVCiRzfojIDY0bMrHUtWC+oVro1XrREnTOOdjqq9Ga0lLZs8nWdICNsKYp8Zzkmjfhc1dKRLKSg==,omsagent.env.clusterName=oms_cluster microsoft/azuremonitor-containers
