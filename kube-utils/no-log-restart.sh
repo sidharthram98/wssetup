f6257143b81edb7eafc3a7c9c7da12fe29a276f0
sidharth <87812643+sidharthram98@users.noreply.github.com>
2022-08-01 20:50:40 +0530

    Update no-log-restart.sh

--- kube-utils/no-log-restart.sh
+++ kube-utils/no-log-restart.sh
@@ -8,12 +8,11 @@ for value in $podList
 do
     echo $value> store.txt
     echo "$(tr "\n,[]'" " " < store.txt)"
-    podName= sudo microk8s kubectl logs $(tr "\n,[]'" " " < store.txt) --since 1m
-    if [[ $(sudo microk8s kubectl logs $value --since=1m) ]]; then
-        echo "${value} pod is fine!"
+    if [[ $(sudo microk8s kubectl logs $value --since=10m) ]]; then
+        echo "-------------------------------${value} pod is fine!--------------------------------------"
         continue
     else
-        echo "logs not found --- Restarting Pod ${value}"
+        echo "-----------------------------logs not found --- Restarting Pod ${value}-------------------------------------"
         sudo microk8s kubectl delete pod $value
     fi
 done
