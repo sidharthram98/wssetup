ee1383da5d1609c2edf99bc416cc5be3eb0ee9b0
sidharth <87812643+sidharthram98@users.noreply.github.com>
2022-08-01 16:18:05 +0530

    Update kubectl.sh

--- kube-utils/kubectl.sh
+++ kube-utils/kubectl.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-podList=$(ls -A | awk 'NR == 1{o = "['\''" $1 "'\''"}NR > 1{o = o ", '\''" $1 "'\''"}END{o = o "]"; print o}')
-
-echo $podList
+echo ' '>store.txt
+podList=$(sudo microk8s kubectl get pods | awk '{print $1}')
+#echo $podList
 
 for value in $podList
 do
-    echo $value> store.txt  
-    podName= tr ",[]'" " " < store.txt
-    if [[ sudo microk8s kubectl logs $podName --since 1m ]]; then
-        echo "logs found"
+    echo $value> store.txt
+    echo "$(tr "\n,[]'" " " < store.txt)"
+    podName= sudo microk8s kubectl logs $(tr "\n,[]'" " " < store.txt) --since 1m
+    if [[ $(sudo microk8s kubectl logs $value --since=1m) ]]; then
+        echo "${value} pod is fine!"
+        continue
     else
-        sudo microk8s kubectl get pods 
+        echo "logs not found --- Restarting Pod ${value}"
+        sudo microk8s kubectl delete pod $value
     fi
 done
