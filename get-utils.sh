83b071855cdd7f2ae3ed53063ae09f884013e64e
sidharth <87812643+sidharthram98@users.noreply.github.com>
2022-08-01 20:40:40 +0530

    Update get-utils.sh

--- get-utils.sh
+++ get-utils.sh
@@ -5,3 +5,7 @@ wget https://raw.githubusercontent.com/sidharthram98/wssetup/main/configmicrok8s
 wget https://raw.githubusercontent.com/sidharthram98/wssetup/main/delete-image.sh
 wget https://raw.githubusercontent.com/sidharthram98/wssetup/main/oms-agent.sh
 wget https://raw.githubusercontent.com/sidharthram98/wssetup/main/.env-template -O .env
+wget https://raw.githubusercontent.com/sidharthram98/wssetup/main/kube-utils/no-log-restart.sh
+wget https://raw.githubusercontent.com/sidharthram98/wssetup/main/kube-utils/store.txt
+
+
