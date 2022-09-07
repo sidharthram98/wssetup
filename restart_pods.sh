f58e815e004cce38997d2414cb50513f7a8c0973
sidharth <87812643+sidharthram98@users.noreply.github.com>
2022-09-07 13:12:28 +0530

    Update restart_pods.sh

--- restart_pods.sh
+++ restart_pods.sh
@@ -1,22 +1,23 @@
 #! /bin/bash
-microk8s kubectl get pods | awk '/Running/' | awk {'print $1'} | tee pods.txt
-
+/usr/local/bin/kubectl get pods | awk '/Running/' | awk {'print $1'} | tee pods.txt
 while read pod; do
-  getLogs=$(microk8s kubectl logs $pod --since 1m)
-
+  getLogs=$(/usr/local/bin/kubectl logs $pod --since 1m)
     if [ "$getLogs"!="" ]; then
-       echo "${pod} pod is fine!"
+       echo "${pod} pod is working fine!">>restart_pods.log
        continue
     else
-      echo "logs not found --- Restarting Pod ${pod}"
-      microk8s kubectl delete pod $pod
+      echo "logs not found --- Restarting Pod" $pod>>restart_pods.log
+      kubectl delete pod $pod
     fi
 done<pods.txt
 
 
-microk8s kubectl get pods | awk '/CrashLoopBackOff/' | awk {'print $1'} | tee pods.txt
+/usr/local/bin/kubectl get pods | awk '/CrashLoopBackOff/' | awk {'print $1'} | tee pods.txt
 while read pod; do
-  microk8s kubectl delete $pod
-  echo 'Restarting ${pod}'
+  kubectl delete $pod
+  echo 'Restarting '$pod>>restart_pods.log
 done<pods.txt
+echo '--------------------------------'>>restart_pods.log
+date | tee -a restart_pods.log
+echo '--------------------------------------------------------------'>>restart_pods.log
 exit 0
