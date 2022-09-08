ec4cc2ce3c82ea2d41ea59ca302d9d235459b1f7
sidharth <87812643+sidharthram98@users.noreply.github.com>
2022-09-08 19:45:55 +0530

    Update cron_expressions.sh

--- cron_expressions.sh
+++ cron_expressions.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 (crontab -l; echo "*/5 * * * * cd /home/anicca/Downloads && bash restart_pods.sh")|crontab -
-(crontab -l; echo "0 */24 * * * echo '------------------Restarting all pods past 24th hour----------------------'>>/home/Downloads/restart_pods.log")|crontab -
-(crontab -l; echo "0 */24 * * * microk8s kubectl delete pods --all | tee -a /home/anicca/Downloads/restart_pods.log && date | tee -a /home/anicca/Downloads/restart_pods.log")|crontab -
-(crontab -l; echo "1 */24 * * * echo '--------------------------------------------------------------------------'>>/home/anicca/Downloads/restart_pods.log")|crontab -
+(crontab -l; echo "0 */24 * * *  cd /home/anicca/Downloads && bash restart_24th.sh")|crontab -
+
